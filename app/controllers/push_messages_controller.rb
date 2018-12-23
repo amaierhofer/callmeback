789057693611ec84357507d653983060851b0b48
Andreas Maierhofer <a.maierhofer@gmail.com>
2018-12-23 17:20:38 +0100

    Use flash to inform about push queueing result

--- app/controllers/push_messages_controller.rb
+++ app/controllers/push_messages_controller.rb
@@ -1,5 +1,9 @@
 class PushMessagesController < ApplicationController
   def create
     client.push
+    flash.now[:notice] = 'Push message enqueued'
+  rescue => e
+    flash.now[:alert] = 'Error enqueueing message'
+    logger.error(e)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the css class for the given flash level.
+  def custom_flash_class(level)
+    case level
+    when :notice then 'success'
+    when :alert then 'danger'
+    else level.to_s
+    end
+  end
 end
